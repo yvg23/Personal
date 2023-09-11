59b90496e43b68ecc81ca7b49d3dbc4fa9877ef8
yashvardhan goyal <103553876+yvg23@users.noreply.github.com>
2023-09-11 17:53:59 +0530

    Create git.py

---
+++ git.py
@@ -0,0 +1,29 @@
+import os
+import random
+from datetime import datetime, timedelta
+
+# Define the start and end dates
+start_date = datetime(2022, 4, 15)
+end_date = datetime(2023, 9, 10)  # One day before September 11, 2023
+
+for i in range(200):
+    # Generate a random date within the specified range
+    rand_days = random.randint(0, (end_date - start_date).days)
+    commit_date = start_date + timedelta(days=rand_days)
+    
+    # Format the date as 'YYYY-MM-DD'
+    formatted_date = commit_date.strftime('%Y-%m-%d')
+    
+    # Generate a commit message with the formatted date
+    commit_message = f"Commit on {formatted_date}"
+    
+    # Append the commit message to 'test.txt'
+    with open('test.txt', 'a') as file:
+        file.write(commit_message + '\n')
+    
+    # Stage and commit the changes using Git
+    os.system('git add test.txt')
+    os.system(f'git commit --date="{commit_date}" -m "{commit_message}"')
+
+# Push the commits to the remote repository
+os.system('git push -u origin main')
